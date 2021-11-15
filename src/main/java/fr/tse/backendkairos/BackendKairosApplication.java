c1821e51eff0d2b35709d591b6d8b9a010d6ad5b
Kamil CAGLAR <kamilcaglar.contact@gmail.com>
2021-11-15 10:41:57 +0100

    initial commit

---
+++ src/main/java/fr/tse/backendkairos/BackendKairosApplication.java
@@ -0,0 +1,13 @@
+package fr.tse.backendkairos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendKairosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendKairosApplication.class, args);
+    }
+
+}
